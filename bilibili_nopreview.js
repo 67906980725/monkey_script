e3fba024162de376ee761bf1ab9a03ecb73b5890
v <v>
2024-06-27 16:53:28 +0800

    update

---
+++ bilibili_nopreview.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         bilibili禁止首页视频自动预览
+// @namespace    http://tampermonkey.net/
+// @version      2024-06-27
+// @description  禁止bilibili首页鼠标悬停时自动预览视频
+// @author       v
+// @match        https://www.bilibili.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict'
+  document.addEventListener('mouseenter', function(event) {
+    event.stopPropagation()
+    event.stopImmediatePropagation()
+    return
+  }, true)
+})()
