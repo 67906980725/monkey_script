8cb71a7b7ff112dbcc6684b452697915cbe19220
v <v>
2024-06-29 16:12:19 +0800

    update

--- bilibili_nopreview.js
+++ bilibili_nopreview.js
@@ -2,7 +2,7 @@
 // @name         bilibili禁止首页视频自动预览
 // @namespace    http://tampermonkey.net/
 // @version      2024-06-27
-// @description  禁止bilibili首页鼠标悬停时自动预览视频
+// @description  bilibili禁止首页视频自动预览
 // @author       v
 // @match        https://www.bilibili.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
@@ -11,11 +11,12 @@
 
 (function() {
   'use strict'
-
-  // div class="bili-video-card__image--wrap" mouseenter
   document.addEventListener('mouseenter', function(event) {
-    event.stopPropagation()
-    event.stopImmediatePropagation()
-    return
+    // div class="bili-video-card__image--wrap" mouseenter
+    if (event.target.classList.contains('bili-video-card__image--wrap')) {
+      event.stopPropagation()
+      event.stopImmediatePropagation()
+      return
+    }
   }, true)
 })()
